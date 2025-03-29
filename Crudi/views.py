9be90d740926096697ecfdb464b890564c32b027
janakiraman <104431918+janakiraman5@users.noreply.github.com>
2025-03-29 22:01:15 +0530

    Update views.py
    v2

--- Crudi/views.py
+++ Crudi/views.py
@@ -5,3 +5,7 @@ from Crudi.models import Student
 def retireview_list(request):
     student = Student.objects.all()
     return render(request, 'Crudi/templates/index.html',{'student':student})
+
+def Create_view(request):
+    forms = StudentForm
+    return render(request,'Crudi/Create.html',{form : form})
